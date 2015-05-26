fc8e1ead84d862294605628fb9cfdd46c21eb729
KIPdeKIP <kipdekip@gmail.com>
2015-05-26 14:46:43 +0200

    Vanuit de folder in plaats van de root

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Bootstrap 101 Template</title>
-		<link href="/css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<!--[if lt IE 9]>
 			<script src="/js/html5shiv.min.js"></script>
 			<script src="/js/respond.min.js"></script>
@@ -14,7 +14,7 @@
 	<body>
 		<div class="container">
 			<header role="banner">
-				<img id="logo-main" src="/js/holder.js/140x140">
+				<img id="logo-main" src="js/holder.js/140x140">
 				<nav id="navbar-primary" class="navbar navbar-default" role="navigation">
 					<div class="container-fluid">
 						<!-- Brand and toggle get grouped for better mobile display -->
@@ -45,8 +45,8 @@
 				<p>Tekst</p>
 			</div>
 		</div>
-		<script src="/js/jquery.min.js"></script>
-		<script src="/js/bootstrap.min.js"></script>
-		<script src="/js/holder.js"></script>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/holder.js"></script>
 	</body>
 </html>
