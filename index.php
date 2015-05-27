e10f8b9c4b844fd3969e5e94ef8774cf0bc31e25
KIPdeKIP <kipdekip@gmail.com>
2015-05-27 11:48:44 +0200

    disable link

--- index.php
+++ index.php
@@ -74,7 +74,7 @@
 				<h1>Example page header <small>Subtext for header</small></h1>
 			</div>
 			<p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.</p>
-			<p>Use <a href="../sticky-footer-navbar">the sticky footer with a fixed navbar</a> navbar if need be, too.</p>
+			<p>Use <a href="#">the sticky footer with a fixed navbar</a> navbar if need be, too.</p>
 			<footer class="footer">
 				<p>&#169; Company 2014</p>
 			</footer>
