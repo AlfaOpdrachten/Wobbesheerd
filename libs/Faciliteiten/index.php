42bf1127f8cc69dc5f26fec86fb74105a5429f92
Klaasjan <klaasjanoosterveld1995@gmail.com>
2015-05-28 11:52:16 +0200

    tabs gezet

--- libs/Faciliteiten/index.php
+++ libs/Faciliteiten/index.php
@@ -5,76 +5,41 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Wobbesheerd :: Paardenstalling Wobbesheerd Oostwold</title>
 </head>
-<body>
-<div id="container">
+	<body>
+		<div id="container">
 
-<div id="header">
-</div>
+			<div id="header">
+			</div>
 
-<div id="content_wrapper">
-<div id="content_wrapper_top">
-<div id="content_wrapper_bottom">
-<div id="content">
+	
+				<div id="content">
 
-<div id="menu">
-<ul>
-<li><a href="/libs/index.php/">Homepage</a></li>
-<li><em><a href="/libs/Faciliteiten/index.php">Faciliteiten</a></em>
-<ul>
-<li><a href="/libs/Faciliteiten/slideshow/boerderij.php">De boerderij</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/Overdekt.php">Overdekte rijhal</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/Stapmolen.php">Stapmolen</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/GroteBBak.php">Grote buitenbak</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/KleineBBak.php">Kleine buitenbak</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/Paddock.php">Paddock</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/GroepstalJong.php">Groepsstal voor jonge paarden</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/indi30.php">Individuele stallen (30 boxen)</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/indi10.php">Individuele stallen (10 boxen)</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/Groepstal.php">Groepsstal</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/Weide.php">Weide</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/Trailer.php">Trailerstalling</a></li>
-</ul>
-</li>
-<li><a href="/libs/Uw-visie/">Uw visie</a></li>
-<li><a href="/libs/Contact/">Contact</a></li>
-<li><a href="/libs/Route/">Route</a></li>
-<li><a href="/libs/Omgeving/">Omgeving</a></li>
-</ul>
-</div>
-
-<div id="text">
-<h1>Faciliteiten</h1>
-<p><img class="alignright" src="/upload/DSC_0079.jpg" alt="" width="500" height="332"></p>
-<ul>
-<li><a href="/libs/Faciliteiten/slideshow/boerderij.php">De boerderij</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/Overdekt.php">Overdekte rijhal</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/Stapmolen.php">Stapmolen</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/GroteBBak.php">Grote buitenbak</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/KleineBBak.php">Kleine buitenbak</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/Paddock.php">Paddock</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/GroepstalJong.php">Groepsstal voor jonge paarden</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/indi30.php">Individuele stallen (30 boxen)</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/indi10.php">Individuele stallen (10 boxen)</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/Groepstal.php">Groepsstal</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/Weide.php">Weide</a></li>
-<li><a href="/libs/Faciliteiten/slideshow/Trailer.php">Trailerstalling</a></li>
-</div>
-
-</div>
-</div>
-</div>
-</div>
-
-<div id="footer">
-<address>
-Hoofdstraat 216<br>
-9828 PD Oostwold<br>
-Tel: 050-5515623<br>
-Mob: 06-15596766<br>
-Email: <span class="email"><a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#105;&#110;&#102;&#111;&#64;&#119;&#111;&#98;&#98;&#101;&#115;&#104;&#101;&#101;&#114;&#100;&#46;&#110;&#108;">&#105;&#110;&#102;&#111;&#64;&#119;&#111;&#98;&#98;&#101;&#115;&#104;&#101;&#101;&#114;&#100;&#46;&#110;&#108;</a></span>
-</address>
-</div>
-
-</div>
-</body>
+						<div id="menu">
+							<ul>
+								<li><a href="../libs/index.php/">Homepage</a></li>
+									<li><em><a href="../libs/Faciliteiten/index.php">Faciliteiten</a></em>
+										<ul>
+											<li><a href="../libs/Faciliteiten/slideshow/boerderij.php">De boerderij</a></li>
+											<li><a href="../libs/Faciliteiten/slideshow/Overdekt.php">Overdekte rijhal</a></li>
+											<li><a href="../libs/Faciliteiten/slideshow/Stapmolen.php">Stapmolen</a></li>
+											<li><a href="../libs/Faciliteiten/slideshow/GroteBBak.php">Grote buitenbak</a></li>
+											<li><a href="../libs/Faciliteiten/slideshow/KleineBBak.php">Kleine buitenbak</a></li>
+											<li><a href="../libs/Faciliteiten/slideshow/Paddock.php">Paddock</a></li>
+											<li><a href="../libs/Faciliteiten/slideshow/GroepstalJong.php">Groepsstal voor jonge paarden</a></li>
+											<li><a href="../libs/Faciliteiten/slideshow/indi30.php">Individuele stallen (30 boxen)</a></li>
+											<li><a href="../libs/Faciliteiten/slideshow/indi10.php">Individuele stallen (10 boxen)</a></li>
+											<li><a href="../libs/Faciliteiten/slideshow/Groepstal.php">Groepsstal</a></li>
+											<li><a href="../libs/Faciliteiten/slideshow/Weide.php">Weide</a></li>
+											<li><a href="../libs/Faciliteiten/slideshow/Trailer.php">Trailerstalling</a></li>
+										</ul>
+									</li>
+								<li><a href="../libs/Uw-visie/">Uw visie</a></li>
+								<li><a href="../libs/Contact/">Contact</a></li>
+								<li><a href="../libs/Route/">Route</a></li>
+								<li><a href="../libs/Omgeving/">Omgeving</a></li>
+							</ul>
+						</div>
+				</div>
+		</div>
+	</body>
 </html>
--- libs/Faciliteiten/slideshow/Groepstal.php
+++ libs/Faciliteiten/slideshow/Groepstal.php
@@ -4,22 +4,6 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Wobbesheerd :: Paardenstalling Wobbesheerd Oostwold</title>
-<link href="/css/normalize/normalize.css" type="text/css" rel="stylesheet">
-<link href="/css/style.css" type="text/css" rel="stylesheet">
-<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
-<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
-<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-<link rel="manifest" href="/manifest.json">
 <meta name="msapplication-TileColor" content="#da532c">
 <meta name="msapplication-TileImage" content="/mstile-144x144.png">
 <meta name="theme-color" content="#ffffff">
@@ -27,144 +11,95 @@
 <body>
 <div id="container">
 
-<div id="header">
-</div>
+    <div id="header">
+    </div>
 
-<div id="content_wrapper">
-<div id="content_wrapper_top">
-<div id="content_wrapper_bottom">
-<div id="content">
+        <div id="content">
 
-    <style>
-        article{
+            <style>
+                article{
 
-            position: fixed;
-            width: 70%;
-            left: 0%;
-            right: 0%;
-            top: 0;
-            bottom: 0;
-            padding: 30px 15%;
-            overflow: auto;
-            z-index: 0;
-            transform-origin: 0 50%;
+                    position: fixed;
+                    width: 70%;
+                    left: 0%;
+                    right: 0%;
+                    top: 0;
+                    bottom: 0;
+                    padding: 30px 15%;
+                    overflow: auto;
+                    z-index: 0;
+                    transform-origin: 0 50%;
 
-        }
+                }
 
-        article:after {
-            position: absolute;
-            content: "";
-            left: 100%
-        top: 0%;
-            right: 0;
-            bottom: 0;
-        }
+                article:after {
+                    position: absolute;
+                    content: "";
+                    left: 100%
+                top: 0%;
+                    right: 0;
+                    bottom: 0;
+                }
 
-        nav {
+                nav {
 
-            position: fixed;
-            left: -15em;
-            top: 0;
-            bottom: 0;
-            background-color: #234729;
-            border-right: 40px solid darkgreen;
-            box-shadow: 5px 0 5px rgba(0,0,0,0.2);
-            z-index: 1;
-            cursor: pointer;
-        }
+                    position: fixed;
+                    left: -15em;
+                    top: 0;
+                    bottom: 0;
+                    background-color: #234729;
+                    border-right: 40px solid darkgreen;
+                    box-shadow: 5px 0 5px rgba(0,0,0,0.2);
+                    z-index: 1;
+                    cursor: pointer;
+                }
 
-        nav:after {
+                nav:after {
 
-            position: absolute;
-            content: "";
-            width: 0;
-            height: 0;
-            right: -70px;
-            top: 50%;
-            border-width: 15px 15px;
-            border-style: solid;
-            border-color: transparent;
-        }
+                    position: absolute;
+                    content: "";
+                    width: 0;
+                    height: 0;
+                    right: -70px;
+                    top: 50%;
+                    border-width: 15px 15px;
+                    border-style: solid;
+                    border-color: transparent;
+                }
 
-        nav ul {
+                nav ul {
 
-            width: 15em;
-            list-style-type: none;
-            font-size: 20px;
-        }
+                    width: 15em;
+                    list-style-type: none;
+                    font-size: 20px;
+                }
 
-        article, article:after, nav, nav * {
-            transition: all 1000ms ease;
+                article, article:after, nav, nav * {
+                    transition: all 1000ms ease;
 
-        }
+                }
 
-        nav:hover {
+                nav:hover {
 
-            left:0;
+                    left:0;
 
-        }
+                }
 
-        nav:hover - article:after {
-                        left: 60%;
-                    }
+                nav:hover - article:after {
+                            left: 60%;
+                        }
 
-        nav li{
+                nav li{
 
-            color: black;
-            padding: 5%;
-        }
+                    color: black;
+                    padding: 5%;
+                }
 
-    </style>
+            </style>
 
-    <menu>
-
-        <li><a href="/id/1-Homepage/">Homepage</a></li>
-        <li><a href="/id/8-Faciliteiten/">Faciliteiten</a>
-            <ul>
-                <li><a href="/id/9-De-boerderij/">De boerderij</a></li>
-                <li><a href="/id/22-Overdekte-rijhal/">Overdekte rijhal</a></li>
-                <li><a href="/id/24-Stapmolen/">Stapmolen</a></li>
-                <li><a href="/id/10-Grote-buitenbak/">Grote buitenbak</a></li>
-                <li><a href="/id/16-Kleine-buitenbak/">Kleine buitenbak</a></li>
-                <li><a href="/id/14-Paddock/">Paddock</a></li>
-                <li><a href="/id/11-Groepsstal-voor-jonge-paarden/">Groepsstal voor jonge paarden</a></li>
-                <li><em ><a href="/id/15-Individuele-stallen-30-boxen/">Individuele stallen (30 boxen)</a></em></li>
-                <li><a href="/id/12-Individuele-stallen-10-boxen/">Individuele stallen (10 boxen)</a></li>
-                <li><a href="/id/13-Groepsstal/">Groepsstal</a></li>
-                <li><a href="/id/17-Weide/">Weide</a></li>
-                <li><a href="/id/18-Trailerstalling/">Trailerstalling</a></li>
-            </ul>
-        </li>
-        <li><a href="/id/19-Uw-visie/">Uw visie</a></li>
-        <li><a href="/id/21-Contact/">Contact</a></li>
-        <li><a href="/id/20-Route/">Route</a></li>
-        <li><a href="/id/23-Omgeving/">Omgeving</a></li>
-        </ul>
-
-    </menu>
-
-<div id="text">
-<h1>Groepsstal</h1>
-<p>De ronde stal is een zeer ruime stal waar we een groep van 10-12 paarden zich kunnen huisvesten. Deze groepsstal is bedoeld voor veulens tot 3 jarigen.</p>
-<p>&nbsp;</p>
-<p><img class="alignright" src="/upload/DSC_0002.jpg" alt="" width="664" height="454"></p>
-</div>
+        </div>
 
 </div>
-</div>
-</div>
-</div>
 
-<div id="footer">
-<address>
-Hoofdstraat 216<br>
-9828 PD Oostwold<br>
-Tel: 050-5515623<br>
-Mob: 06-15596766<br>
-Email: <span class="email"><a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#105;&#110;&#102;&#111;&#64;&#119;&#111;&#98;&#98;&#101;&#115;&#104;&#101;&#101;&#114;&#100;&#46;&#110;&#108;">&#105;&#110;&#102;&#111;&#64;&#119;&#111;&#98;&#98;&#101;&#115;&#104;&#101;&#101;&#114;&#100;&#46;&#110;&#108;</a></span>
-</address>
-</div>
-
-</div>
 </body>
 </html>
