4ab38fc247779728f69babe720a44615b34b4590
KIPdeKIP <kipdekip@gmail.com>
2015-05-28 10:20:22 +0200

    Add page content

--- index.php
+++ index.php
@@ -58,10 +58,42 @@
 				</nav>
 			</header><!-- header role="banner" -->
 			<div class="page-header">
-				<h1>Example page header <small>Subtext for header</small></h1>
+				<h1>Contact</h1>
 			</div>
-			<p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.</p>
-			<p>Use <a href="#">the sticky footer with a fixed navbar</a> navbar if need be, too.</p>
+			<p><img class="alignright" src="upload/contact.jpg" alt="Contact" width="300" height="199"></p>
+			<p>S.R.&nbsp;Wobbes</p>
+			<p>Hoofdstraat 216<br> 9828 PD Oostwold (gem Leek, Gr.)<br> Tel: 050-5515623<br> Mob: 06-15596766</p>
+			<p>email: <a href="mailto:info@wobbesheerd.nl">info@wobbesheerd.nl</a></p>
+			<p>Als u vragen heeft of op zoek bent naar verdere mogelijkheden, neemt u dan contact op met ons. Wij staan u graag te woord.</p>
+			<p>Met vriendelijke groet,<br>Reinier Wobbes</p>
+			<p><a href="mailto:info@wobbesheerd.nl">info@wobbesheerd.nl</a></p>
+			<p>* <span style="font-size: xx-small;">Is niet verplicht&nbsp;</span></p>
+			<form name="FormHandler" method="post" action="./bestanden/Wobbesheerd __ Contact.html">
+			<input type="hidden" name="FormHandler_submit" id="FormHandler_submit" value="1">
+
+			<table border="0" cellspacing="0" cellpadding="3">
+			<tr> 
+				<td valign="top">Uw naam : &nbsp;</td> 
+				<td valign="top"><input type="text" name="Uwnaam" id="Uwnaam" value="" size="20">   &nbsp;</td> 
+			</tr>
+			<tr> 
+				<td valign="top">Email : &nbsp;</td> 
+				<td valign="top"><input type="text" name="Email" id="Email" value="" size="30">   &nbsp;</td> 
+			</tr>
+			<tr> 
+				<td valign="top">Telefoon *  : &nbsp;</td> 
+				<td valign="top"><input type="text" name="Telefoon" id="Telefoon" value="" size="20">   &nbsp;</td> 
+			</tr>
+			<tr> 
+				<td valign="top">Bericht : &nbsp;</td> 
+				<td valign="top"><textarea name="Bericht" id="Bericht" cols="40" rows="10"></textarea>   &nbsp;</td> 
+			</tr>
+			<tr> 
+				<td valign="top">  &nbsp;</td> 
+				<td valign="top"><input type="submit" value="Verzend" name="button1" id="button1">   &nbsp;</td> 
+			</tr>
+			</table>
+			</form>
 			<footer class="footer">
 				<p>&#169; Company 2014</p>
 			</footer>
