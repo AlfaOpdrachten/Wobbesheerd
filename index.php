5106cddcc64f5fac8d5990df0d29fc3a25792e3f
Reinderh <reinderh@gmail.com>
2015-05-28 14:25:03 +0200

    contactformulier
    Nieuwe opmaak

--- index.php
+++ index.php
@@ -70,16 +70,32 @@
 				<p><a href="&#x6d;&#97;&#x69;&#x6c;&#x74;&#x6f;&#58;&#x69;&#x6e;&#x66;&#x6f;&#64;&#x77;&#x6f;&#98;&#98;&#x65;&#x73;&#x68;&#x65;&#x65;&#x72;&#x64;&#46;&#x6e;&#x6c;">&#x69;&#x6e;&#x66;&#x6f;&#64;&#x77;&#x6f;&#98;&#98;&#x65;&#x73;&#x68;&#x65;&#x65;&#x72;&#x64;&#46;&#x6e;&#x6c;</a></p>
 				<p>* <span style="font-size: xx-small;">Is niet verplicht&nbsp;</span></p>
                 <form method="post" action="formulierverzenden.php">
-                    <ul>
-                        <li>Uw naam *</li>
-                        <li><input type="text" name="Naam" placeholder="Naam"/></li>
-                        <li>Uw e-mail adres *</li>
-                        <li><input type="text" name="email" /></li>
-                        <li>Uw berichtje *</li>
-                        <li><textarea cols="50" rows="4" name="bericht"></textarea></li>
-                        <li><input type="reset" value="Begin opnieuw" /> <input type="submit" name="versturen" value="Verstuur bericht" /></li>
-                    </ul>
-                    <p>Alle velden gemarkeerd met een * zijn verplicht.</p>
+                    <TABLE BORDER="1" CELLSPACING="">
+                        <TR>
+                            <TD>Naam</TD>
+                            <TD>
+                                <INPUT TYPE="TEXT" NAME="naam" SIZE="30" placeholder="Naam">
+                            </TD>
+                        </TR>
+                        <TR>
+                            <TD>E-Mail adres&nbsp;</TD>
+                            <TD><INPUT TYPE="TEXT" NAME="Mail" SIZE="30" placeholder="voorbeeld@mail.nl"></TD>
+                        </TR>
+                        <TR>
+                            <TD>Telefoon nummer</TD>
+                            <TD>
+                                <input type="text" name="nummer" size="30" placeholder="0612345678">
+                            </TD>
+                        </TR>
+                        <TR>
+                            <TD>Uw berichtje</TD>
+                            <TD>
+                                <textarea cols="50" rows="4" name="bericht" placeholder="Typ hier uw bericht"></textarea>
+                            </TD>
+                        </TR>
+                    </TABLE><br>
+                    <input type="submit" name="versturen" value="Verstuur bericht" />
+
                 </form>
 
 
