e76379d19ad270ebe24bd8299f237023440a703c
Klaasjan <klaasjanoosterveld1995@gmail.com>
2015-06-04 11:28:45 +0200

    delete en aanpassing omgeving.php

---
+++ libs/oldomgeving.php
@@ -0,0 +1,58 @@
+<?php if(isset($_GET['textmode'])){header('Content-Type: text/plain');}?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Wobbesheerd :: Paardenstalling Wobbesheerd Oostwold</title>
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+<meta name="theme-color" content="#ffffff">
+</head>
+<body>
+<div id="container">
+
+<div id="header"> 
+</div>
+
+<div id="content_wrapper">
+<div id="content_wrapper_top">
+<div id="content_wrapper_bottom">
+<div id="content">
+
+<div id="menu">
+<ul>
+<li><a href="index.php">Homepage</a></li>
+<li><a href="Faciliteiten/index.php">Faciliteiten</a></li>
+<li><a href="uwvisie.php">Uw visie</a></li>
+<li><a href="contact.php">Contact</a></li>
+<li><a href="route.php">Route</a></li>
+<li><em><a href="omgeving.php">Omgeving</a></em></li>
+</ul>
+</div>
+
+<div id="text">
+<h1>Omgeving</h1>
+<p>Groningen staat bekend om zijn vele groene streken, zo ook Wobbesheerd. Vanuit Wobbesheerd kunt u vele buitenritten maken met uw paard.</p>
+<p>Wobbesheerd ligt op 10 minuten afstand van Landgoed Nienoord.</p>
+<p>Als u meer informatie zoekt over de mogelijkheden, neemt u dan <a href="/id/21-Contact/">contact</a> met ons op.</p>
+<p><img class="alignright" src="/upload/DSC_0159.jpg" alt="" width="500" height="332"></p>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+
+<div id="footer">
+<address>
+Hoofdstraat 216<br>
+9828 PD Oostwold<br>
+Tel: 050-5515623<br>
+Mob: 06-15596766<br>
+Email: <span class="email"><a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#105;&#110;&#102;&#111;&#64;&#119;&#111;&#98;&#98;&#101;&#115;&#104;&#101;&#101;&#114;&#100;&#46;&#110;&#108;">&#105;&#110;&#102;&#111;&#64;&#119;&#111;&#98;&#98;&#101;&#115;&#104;&#101;&#101;&#114;&#100;&#46;&#110;&#108;</a></span>
+</address>
+</div>
+
+</div>
+</body>
+</html>
