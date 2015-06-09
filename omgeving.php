72a7f0fc593d4867be42da5817ec18197799eebf
KIPdeKIP <kipdekip@gmail.com>
2015-06-09 13:55:16 +0200

    Move omgeving.php

---
+++ omgeving.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="nl">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Bootstrap 101 Template</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/bootstrap-override.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+			<script src="js/html5shiv.min.js"></script>
+			<script src="js/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="container">
+			<header role="banner">
+				<img id="logo-main" src="img/header.jpg" width="900" height="201" title="Paardenstalling Wobbesheerd Oostwold" alt="Paardenstalling Wobbesheerd Oostwold">
+				<nav id="navbar-primary" class="navbar navbar-default">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Wobbesheerd</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav">
+								<li><a href="#">Faciliteiten</a></li>
+								<li><a href="#">Uw visie</a></li>
+								<li class="active"><a href="#">Contact</a></li>
+								<li><a href="../Wobbesheerd/libs/route.php">Route</a></li>
+								<li><a href="#">Omgeving <span class="sr-only">(huidige)</span></a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			</header><!-- header role="banner" -->
+			<div class="page-content">
+				<div class="page-header">
+					<h1>Omgeving</h1>
+				</div>
+				<p>Groningen staat bekend om zijn vele groene streken, zo ook Wobbesheerd. Vanuit Wobbesheerd kunt u vele buitenritten maken met uw paard.</p>
+				<p>Wobbesheerd ligt op 10 minuten afstand van Landgoed Nienoord.</p>
+				<p>Als u meer informatie zoekt over de mogelijkheden, neemt u dan <a href="contact.php">contact</a> met ons op.</p>
+				<p><img class="alignnone" src="/upload/DSC_0159.jpg" width="500" height="332"></p>
+			</div><!-- /.page-content -->
+			<footer class="footer">
+				<address>
+					Hoofdstraat 216<br>
+					9828 PD Oostwold<br>
+					Tel: 050-5515623<br>
+					Mob. 06-15596766 <br>
+					Email: <span class="email"><a href="&#x6d;&#97;&#x69;&#x6c;&#x74;&#x6f;&#58;&#x69;&#x6e;&#x66;&#x6f;&#64;&#x77;&#x6f;&#98;&#98;&#x65;&#x73;&#x68;&#x65;&#x65;&#x72;&#x64;&#46;&#x6e;&#x6c;">&#x69;&#x6e;&#x66;&#x6f;&#64;&#x77;&#x6f;&#98;&#98;&#x65;&#x73;&#x68;&#x65;&#x65;&#x72;&#x64;&#46;&#x6e;&#x6c;</a></span><br>
+				</address>	
+			</footer>
+		</div><!-- /.container -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
