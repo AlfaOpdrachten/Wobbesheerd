69ea7f3bf691bbe8edab6352974f05b80f6b10ea
KIPdeKIP <kipdekip@gmail.com>
2015-06-09 14:13:28 +0200

    Add faciliteiten

---
+++ faciliteiten.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="nl">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Bootstrap 101 Template</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/bootstrap-override.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+			<script src="js/html5shiv.min.js"></script>
+			<script src="js/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="container">
+			<header role="banner">
+				<img id="logo-main" src="img/header.jpg" width="900" height="201" title="Paardenstalling Wobbesheerd Oostwold" alt="Paardenstalling Wobbesheerd Oostwold">
+				<nav id="navbar-primary" class="navbar navbar-default">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="index.php">Wobbesheerd</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav">
+								<li class="active"><a href="faciliteiten.php">Faciliteiten <span class="sr-only">(huidige)</span></a></li>
+								<li><a href="uwvisie.php">Uw visie</a></li>
+								<li><a href="contact.php">Contact</a></li>
+								<li><a href="route.php">Route</a></li>
+								<li><a href="omgeving.php">Omgeving</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			</header><!-- header role="banner" -->
+			<div class="page-content">
+				<div class="page-header">
+					<h1>Faciliteiten</h1>
+				</div>
+				<p>Tekst</p>
+			</div><!-- /.page-content -->
+			<footer class="footer">
+				<address>
+					Hoofdstraat 216<br>
+					9828 PD Oostwold<br>
+					Tel: 050-5515623<br>
+					Mob. 06-15596766 <br>
+					Email: <span class="email"><a href="&#x6d;&#97;&#x69;&#x6c;&#x74;&#x6f;&#58;&#x69;&#x6e;&#x66;&#x6f;&#64;&#x77;&#x6f;&#98;&#98;&#x65;&#x73;&#x68;&#x65;&#x65;&#x72;&#x64;&#46;&#x6e;&#x6c;">&#x69;&#x6e;&#x66;&#x6f;&#64;&#x77;&#x6f;&#98;&#98;&#x65;&#x73;&#x68;&#x65;&#x65;&#x72;&#x64;&#46;&#x6e;&#x6c;</a></span><br>
+				</address>	
+			</footer>
+		</div><!-- /.container -->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
