23d00c9e04819cf0dfd3316fc50cce9ede6027ec
KIPdeKIP <kipdekip@gmail.com>
2015-06-09 14:18:22 +0200

    fix image

--- omgeving.php
+++ omgeving.php
@@ -49,7 +49,7 @@
 				<p>Groningen staat bekend om zijn vele groene streken, zo ook Wobbesheerd. Vanuit Wobbesheerd kunt u vele buitenritten maken met uw paard.</p>
 				<p>Wobbesheerd ligt op 10 minuten afstand van Landgoed Nienoord.</p>
 				<p>Als u meer informatie zoekt over de mogelijkheden, neemt u dan <a href="contact.php">contact</a> met ons op.</p>
-				<p><img class="alignnone" src="/upload/DSC_0159.jpg" width="500" height="332"></p>
+				<p><img class="alignnone" src="upload/DSC_0159.jpg" width="500" height="332"></p>
 			</div><!-- /.page-content -->
 			<footer class="footer">
 				<address>
