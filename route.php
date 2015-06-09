5eb78d500a948caae8b03f71ef298ef7ed311045
KIPdeKIP <kipdekip@gmail.com>
2015-06-09 13:59:47 +0200

    Move files

--- route.php
+++ route.php
@@ -5,17 +5,17 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Bootstrap 101 Template</title>
-		<link href="../css/bootstrap.min.css" rel="stylesheet">
-		<link href="../css/bootstrap-override.css" rel="stylesheet">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/bootstrap-override.css" rel="stylesheet">
 		<!--[if lt IE 9]>
-			<script src="../js/html5shiv.min.js"></script>
-			<script src="../js/respond.min.js"></script>
+			<script src="js/html5shiv.min.js"></script>
+			<script src="js/respond.min.js"></script>
 		<![endif]-->
 	</head>
 	<body>
 		<div class="container">
 			<header role="banner">
-				<img id="logo-main" src="../img/header.jpg" width="900" height="201" title="Paardenstalling Wobbesheerd Oostwold" alt="Paardenstalling Wobbesheerd Oostwold">
+				<img id="logo-main" src="img/header.jpg" width="900" height="201" title="Paardenstalling Wobbesheerd Oostwold" alt="Paardenstalling Wobbesheerd Oostwold">
 				<nav id="navbar-primary" class="navbar navbar-default">
 					<div class="container-fluid">
 						<!-- Brand and toggle get grouped for better mobile display -->
@@ -26,17 +26,17 @@
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
 							</button>
-							<a class="navbar-brand" href="../#">Wobbesheerd</a>
+							<a class="navbar-brand" href="#">Wobbesheerd</a>
 						</div>
 
 						<!-- Collect the nav links, forms, and other content for toggling -->
 						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 							<ul class="nav navbar-nav">
-								<li><a href="../#">Faciliteiten</a></li>
-								<li><a href="../#">Uw visie</a></li>
-								<li><a href="../#">Contact</a></li>
+								<li><a href="#">Faciliteiten</a></li>
+								<li><a href="#">Uw visie</a></li>
+								<li><a href="#">Contact</a></li>
 								<li class="active"><a href="#">Route <span class="sr-only">(current)</span></a></li>
-								<li><a href="../#">Omgeving</a></li>
+								<li><a href="#">Omgeving</a></li>
 							</ul>
 						</div><!-- /.navbar-collapse -->
 					</div><!-- /.container-fluid -->
@@ -77,7 +77,7 @@
 				</address>	
 			</footer>
 		</div><!-- /.container -->
-		<script src="../js/jquery.min.js"></script>
-		<script src="../js/bootstrap.min.js"></script>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
 	</body>
 </html>
