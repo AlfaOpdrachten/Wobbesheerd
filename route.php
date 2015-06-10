c73210d1c949f5e006e9082b8cd8b5c5fca6d15a
KIPdeKIP <kipdekip@gmail.com>
2015-06-10 09:39:48 +0200

    Fix async iframe

--- route.php
+++ route.php
@@ -67,9 +67,9 @@
 					<p>&nbsp;</p>
 					<iframe id="maps" src="" width="600" height="450" frameborder="0" style="border:0"></iframe>
 					<script>
-						$(document).ready(function() {
-							$("maps").src = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d38241.79070055959!2d6.4464!3d53.19790900000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c834c1be77e199%3A0xb214376f3c59e894!2sHoofdstraat+216%2C+9828+PD+Oostwold+Gem+Leek!5e0!3m2!1snl!2snl!4v1433248466347";
-						});
+						window.onload = function(){
+							document.getElementById('maps').src = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d38241.79070055959!2d6.4464!3d53.19790900000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c834c1be77e199%3A0xb214376f3c59e894!2sHoofdstraat+216%2C+9828+PD+Oostwold+Gem+Leek!5e0!3m2!1snl!2snl!4v1433248466347";
+						};
 					</script>
 			</div><!-- /.page-content -->
 			<footer class="footer">
