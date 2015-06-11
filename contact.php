314ad81c734f3fe9ba15609988c240c1c693fe1b
KIPdeKIP <kipdekip@gmail.com>
2015-06-11 11:54:52 +0200

    Required

--- contact.php
+++ contact.php
@@ -61,14 +61,14 @@
 					<div class="form-group">
 						<label class="control-label col-sm-2" for="naam">Naam:</label>
 						<div class="col-sm-10">
-							<input type="text" class="form-control" id="naam" name="naam" placeholder="Naam">
+							<input type="text" class="form-control" id="naam" name="naam" placeholder="Naam" required>
 							<span class="text-danger">*</span>
 						</div>
 					</div>
 					<div class="form-group">
 						<label class="control-label col-sm-2" for="email">E-mail adres:</label>
 						<div class="col-sm-10">          
-							<input type="email" class="form-control" id="email" name="email" placeholder="voorbeeld@mail.nl">
+							<input type="email" class="form-control" id="email" name="email" placeholder="voorbeeld@mail.nl" required>
 							<span class="text-danger">*</span>
 						</div>
 					</div>
@@ -81,7 +81,7 @@
 					<div class="form-group">
 						<label class="control-label col-sm-2" for="bericht">Uw bericht:</label>
 						<div class="col-sm-10">
-							<textarea class="form-control" rows="5" id="bericht" name="bericht" placeholder="Typ hier uw bericht."></textarea>
+							<textarea class="form-control" rows="5" id="bericht" name="bericht" placeholder="Typ hier uw bericht." required></textarea>
 							<span class="text-danger">*</span>
 						</div>
 					</div>
