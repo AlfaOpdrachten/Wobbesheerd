c0c3b7e13b892eca2701df4bad6599cd7dc3f542
KIPdeKIP <kipdekip@gmail.com>
2015-06-22 14:08:06 +0200

    CMS example code

---
+++ CMS.php
@@ -0,0 +1,8 @@
+<?php
+$data = file_get_contents('http://www.caferestauranthetwittepaard.nl/CMS/text/?location=WH.%');
+$json = json_decode($data, 1);
+
+echo '<pre>';
+var_dump($json);
+echo '</pre>'
+?>
