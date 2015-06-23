2dcb8dd23b7a77a36e927b8b37218df0f5bec33c
KIPdeKIP <kipdekip@gmail.com>
2015-06-23 14:06:46 +0200

    Update request.inc.php

--- inc/request.inc.php
+++ inc/request.inc.php
@@ -1,6 +1,4 @@
 <?php
-$data = file_get_contents('http://www.caferestauranthetwittepaard.nl/CMS/text/?location=WH.%');
-$json = json_decode($data, 1);
-echo '<pre>';
-var_dump($json);
-echo '</pre>'
+$json = file_get_contents('http://www.caferestauranthetwittepaard.nl/CMS/text/?location=WH.%');
+$data = json_decode($json, 1);
+var_dump($data);
